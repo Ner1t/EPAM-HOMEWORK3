672a5164927d98cb0ae478503a9f6b30f8f6247b
Roman <roman.nerit@gmail.com>
2020-04-18 15:06:45 +0300

    Add project files.

---
+++ calculator/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                double FirstValue, SecondValue;
+                string action;
+                try
+                {
+                    Console.WriteLine("Введите число 1");
+                    FirstValue = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Введите число 2");
+                    SecondValue = double.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Только числа!");
+                    Console.ReadLine();
+                    continue;
+                }
+            
+
+            Console.WriteLine("Выберите операцию: '+' '-' '/' '*'");
+            action = Console.ReadLine();
+            
+                
+                switch (action)
+                {
+                    case "+":
+                        Console.WriteLine(FirstValue + SecondValue);
+                        break;
+                    case "-":
+                        Console.WriteLine(FirstValue - SecondValue);
+                        break;
+
+                    case "/":
+                        Console.WriteLine(FirstValue / SecondValue);
+                        break;
+
+                    case "*":
+                        Console.WriteLine(FirstValue * SecondValue);
+                        break;
+                    
+
+                    default:
+                        Console.WriteLine("Ошибка, неизвестная операция");
+                        break;
+                };
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
